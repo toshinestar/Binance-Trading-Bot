j-waters/binance-trade-bot

Refactor so that things are easier to mock

binance_trade_bot/auto_trader.py
@@ -9,7 +9,6 @@ from .config import Config
 from .database import Database
 from .logger import Logger
 from .models import Pair, Coin, CoinValue
-from .utils import get_market_ticker_price_from_list
 
 
 class AutoTrader:
@@ -47,7 +46,7 @@ class AutoTrader:
         session: Session
         with self.db.db_session() as session:
             for pair in session.query(Pair).filter(Pair.to_coin == current_coin):
-                from_coin_price = get_market_ticker_price_from_list(all_tickers, pair.from_coin + self.config.BRIDGE)
+                from_coin_price = all_tickers.get_price(pair.from_coin + self.config.BRIDGE)
 
                 if from_coin_price is None:
                     self.logger.info("Skipping update for coin {0} not found".format(pair.from_coin + self.config.BRIDGE))
@@ -68,12 +67,12 @@ class AutoTrader:
                     continue
                 self.logger.info("Initializing {0} vs {1}".format(pair.from_coin, pair.to_coin))
 
-                from_coin_price = get_market_ticker_price_from_list(all_tickers, pair.from_coin + self.config.BRIDGE)
+                from_coin_price = all_tickers.get_price(pair.from_coin + self.config.BRIDGE)
                 if from_coin_price is None:
                     self.logger.info("Skipping initializing {0}, symbol not found".format(pair.from_coin + self.config.BRIDGE))
                     continue
 
-                to_coin_price = get_market_ticker_price_from_list(all_tickers, pair.to_coin + self.config.BRIDGE)
+                to_coin_price = all_tickers.get_price(pair.to_coin + self.config.BRIDGE)
                 if to_coin_price is None:
                     self.logger.info("Skipping initializing {0}, symbol not found".format(pair.to_coin + self.config.BRIDGE))
                     continue
@@ -115,7 +114,7 @@ class AutoTrader:
             datetime.now()) + " - CONSOLE - INFO - I am scouting the best trades. Current coin: {0} ".format(
             current_coin + self.config.BRIDGE), end='\r')
 
-        current_coin_price = get_market_ticker_price_from_list(all_tickers, current_coin + self.config.BRIDGE)
+        current_coin_price = all_tickers.get_price(current_coin + self.config.BRIDGE)
 
         if current_coin_price is None:
             self.logger.info("Skipping scouting... current coin {0} not found".format(current_coin + self.config.BRIDGE))
@@ -126,7 +125,7 @@ class AutoTrader:
         for pair in self.db.get_pairs_from(current_coin):
             if not pair.to_coin.enabled:
                 continue
-            optional_coin_price = get_market_ticker_price_from_list(all_tickers, pair.to_coin + self.config.BRIDGE)
+            optional_coin_price = all_tickers.get_price(pair.to_coin + self.config.BRIDGE)
 
             if optional_coin_price is None:
                 self.logger.info("Skipping scouting... optional coin {0} not found".format(pair.to_coin + self.config.BRIDGE))
@@ -165,8 +164,8 @@ class AutoTrader:
                 balance = self.manager.get_currency_balance(coin.symbol)
                 if balance == 0:
                     continue
-                usd_value = get_market_ticker_price_from_list(all_ticker_values, coin + "USDT")
-                btc_value = get_market_ticker_price_from_list(all_ticker_values, coin + "BTC")
+                usd_value = all_ticker_values.get_price(coin + "USDT")
+                btc_value = all_ticker_values.get_price(coin + "BTC")
                 cv = CoinValue(coin, balance, usd_value, btc_value, datetime=now)
                 session.add(cv)
                 self.db.send_update(cv)

binance_trade_bot/binance_api_manager.py
@@ -1,5 +1,6 @@
 import math
 import time
+from typing import List, Dict
 
 from binance.client import Client
 from binance.exceptions import BinanceAPIException
@@ -8,7 +9,15 @@ from .config import Config
 from .database import Database
 from .logger import Logger
 from .models import Coin
-from .utils import get_market_ticker_price_from_list
+
+
+class AllTickers:
+    def __init__(self, all_tickers: List[Dict]):
+        self.all_tickers = all_tickers
+
+    def get_price(self, ticker_symbol):
+        ticker = next((t for t in self.all_tickers if t[u'symbol'] == ticker_symbol), None)
+        return float(ticker[u'price']) if ticker else None
 
 
 class BinanceAPIManager:
@@ -17,11 +26,11 @@ class BinanceAPIManager:
         self.db = db
         self.logger = logger
 
-    def get_all_market_tickers(self):
+    def get_all_market_tickers(self) -> AllTickers:
         """
         Get ticker price of all coins
         """
-        return self.BinanceClient.get_all_tickers()
+        return AllTickers(self.BinanceClient.get_all_tickers())
 
     def get_market_ticker_price(self, ticker_symbol: str):
         """
@@ -90,10 +99,18 @@ class BinanceAPIManager:
 
         return order_status
 
-    def buy_alt(self, origin_coin: Coin, target_coin: Coin, all_tickers):
+    def buy_alt(self, origin_coin: Coin, target_coin: Coin, all_tickers: AllTickers):
         return self.retry(self._buy_alt, origin_coin, target_coin, all_tickers)
 
-    def _buy_alt(self, origin_coin: Coin, target_coin: Coin, all_tickers):
+    def _buy_quantity(self, target_balance, from_coin_price, origin_symbol, target_symbol):
+        origin_tick = self.get_alt_tick(origin_symbol, target_symbol)
+        return math.floor(
+            target_balance
+            * 10 ** origin_tick
+            / from_coin_price
+        ) / float(10 ** origin_tick)
+
+    def _buy_alt(self, origin_coin: Coin, target_coin: Coin, all_tickers: AllTickers):
         """
         Buy altcoin
         """
@@ -101,17 +118,11 @@ class BinanceAPIManager:
         origin_symbol = origin_coin.symbol
         target_symbol = target_coin.symbol
 
-        origin_tick = self.get_alt_tick(origin_symbol, target_symbol)
-
         origin_balance = self.get_currency_balance(origin_symbol)
         target_balance = self.get_currency_balance(target_symbol)
-        from_coin_price = get_market_ticker_price_from_list(all_tickers, origin_symbol + target_symbol)
+        from_coin_price = all_tickers.get_price(origin_symbol + target_symbol)
 
-        order_quantity = math.floor(
-            target_balance
-            * 10 ** origin_tick
-            / from_coin_price
-        ) / float(10 ** origin_tick)
+        order_quantity = self._buy_quantity(target_balance, from_coin_price, origin_symbol, target_symbol)
         self.logger.info("BUY QTY {0}".format(order_quantity))
 
         # Try to buy until successful
@@ -143,6 +154,12 @@ class BinanceAPIManager:
     def sell_alt(self, origin_coin: Coin, target_coin: Coin):
         return self.retry(self._sell_alt, origin_coin, target_coin)
 
+    def _sell_quantity(self, origin_balance, origin_symbol, target_symbol):
+        origin_tick = self.get_alt_tick(origin_symbol, target_symbol)
+        return math.floor(
+            origin_balance * 10 ** origin_tick
+        ) / float(10 ** origin_tick)
+
     def _sell_alt(self, origin_coin: Coin, target_coin: Coin):
         """
         Sell altcoin
@@ -151,20 +168,17 @@ class BinanceAPIManager:
         origin_symbol = origin_coin.symbol
         target_symbol = target_coin.symbol
 
-        origin_tick = self.get_alt_tick(origin_symbol, target_symbol)
-
-        order_quantity = math.floor(
-            self.get_currency_balance(origin_symbol) * 10 ** origin_tick
-        ) / float(10 ** origin_tick)
-        self.logger.info("Selling {0} of {1}".format(order_quantity, origin_symbol))
-
         origin_balance = self.get_currency_balance(origin_symbol)
         target_balance = self.get_currency_balance(target_symbol)
+
+        order_quantity = self._sell_quantity(origin_balance, origin_symbol, target_symbol)
+        self.logger.info("Selling {0} of {1}".format(order_quantity, origin_symbol))
+
         self.logger.info("Balance is {0}".format(origin_balance))
         order = None
         while order is None:
             order = self.BinanceClient.order_market_sell(
-                symbol=origin_symbol + target_symbol, quantity=(order_quantity)
+                symbol=origin_symbol + target_symbol, quantity=order_quantity
             )
 
         self.logger.info("order")

toshinestar/Binance-Trading-Bot

binance_trade_bot/auto_trader.py
@@ -3,12 +3,11 @@ from typing import Dict, List
 
 from sqlalchemy.orm import Session
 
-from .binance_api_manager import BinanceAPIManager
+from .binance_api_manager import AllTickers, BinanceAPIManager
 from .config import Config
 from .database import Database
 from .logger import Logger
 from .models import Coin, CoinValue, Pair
-from .utils import get_market_ticker_price_from_list
 
 
 class AutoTrader:
@@ -48,7 +47,7 @@ class AutoTrader:
         session: Session
         with self.db.db_session() as session:
             for pair in session.query(Pair).filter(Pair.to_coin == coin):
-                from_coin_price = get_market_ticker_price_from_list(all_tickers, pair.from_coin + self.config.BRIDGE)
+                from_coin_price = all_tickers.get_price(pair.from_coin + self.config.BRIDGE)
 
                 if from_coin_price is None:
                     self.logger.info(
@@ -69,16 +68,16 @@ class AutoTrader:
             for pair in session.query(Pair).filter(Pair.ratio.is_(None)).all():
                 if not pair.from_coin.enabled or not pair.to_coin.enabled:
                     continue
-                self.logger.info(f"Initializing {pair.from_coin} vs {pair.to_coin}", False)
+                self.logger.info(f"Initializing {pair.from_coin} vs {pair.to_coin}")
 
-                from_coin_price = get_market_ticker_price_from_list(all_tickers, pair.from_coin + self.config.BRIDGE)
+                from_coin_price = all_tickers.get_price(pair.from_coin + self.config.BRIDGE)
                 if from_coin_price is None:
                     self.logger.info(
                         "Skipping initializing {}, symbol not found".format(pair.from_coin + self.config.BRIDGE)
                     )
                     continue
 
-                to_coin_price = get_market_ticker_price_from_list(all_tickers, pair.to_coin + self.config.BRIDGE)
+                to_coin_price = all_tickers.get_price(pair.to_coin + self.config.BRIDGE)
                 if to_coin_price is None:
                     self.logger.info(
                         "Skipping initializing {}, symbol not found".format(pair.to_coin + self.config.BRIDGE)
@@ -93,14 +92,14 @@ class AutoTrader:
         """
         raise NotImplementedError()
 
-    def _get_ratios(self, coin: Coin, coin_price, all_tickers):
+    def _get_ratios(self, coin: Coin, coin_price: float, all_tickers: AllTickers):
         """
         Given a coin, get the current price ratio for every other enabled coin
         """
         ratio_dict: Dict[Pair, float] = {}
 
         for pair in self.db.get_pairs_from(coin):
-            optional_coin_price = get_market_ticker_price_from_list(all_tickers, pair.to_coin + self.config.BRIDGE)
+            optional_coin_price = all_tickers.get_price(pair.to_coin + self.config.BRIDGE)
 
             if optional_coin_price is None:
                 self.logger.info(
@@ -122,7 +121,7 @@ class AutoTrader:
             ) - pair.ratio
         return ratio_dict
 
-    def _jump_to_best_coin(self, coin: Coin, coin_price: float, all_tickers):
+    def _jump_to_best_coin(self, coin: Coin, coin_price: float, all_tickers: AllTickers):
         """
         Given a coin, search for a coin to jump to
         """
@@ -145,7 +144,7 @@ class AutoTrader:
         all_tickers = self.manager.get_all_market_tickers()
 
         for coin in self.db.get_coins():
-            current_coin_price = get_market_ticker_price_from_list(all_tickers, coin + self.config.BRIDGE)
+            current_coin_price = all_tickers.get_price(coin + self.config.BRIDGE)
 
             if current_coin_price is None:
                 continue
@@ -174,8 +173,8 @@ class AutoTrader:
                 balance = self.manager.get_currency_balance(coin.symbol)
                 if balance == 0:
                     continue
-                usd_value = get_market_ticker_price_from_list(all_ticker_values, coin + "USDT")
-                btc_value = get_market_ticker_price_from_list(all_ticker_values, coin + "BTC")
+                usd_value = all_ticker_values.get_price(coin + "USDT")
+                btc_value = all_ticker_values.get_price(coin + "BTC")
                 cv = CoinValue(coin, balance, usd_value, btc_value, datetime=now)
                 session.add(cv)
                 self.db.send_update(cv)

binance_trade_bot/binance_api_manager.py
@@ -1,6 +1,6 @@
 import math
 import time
-from typing import Dict
+from typing import Dict, List
 
 from binance.client import Client
 from binance.exceptions import BinanceAPIException
@@ -10,7 +10,15 @@ from .config import Config
 from .database import Database
 from .logger import Logger
 from .models import Coin
-from .utils import get_market_ticker_price_from_list
+
+
+class AllTickers:  # pylint: disable=too-few-public-methods
+    def __init__(self, all_tickers: List[Dict]):
+        self.all_tickers = all_tickers
+
+    def get_price(self, ticker_symbol):
+        ticker = next((t for t in self.all_tickers if t["symbol"] == ticker_symbol), None)
+        return float(ticker["price"]) if ticker else None
 
 
 class BinanceAPIManager:
@@ -54,11 +62,11 @@ class BinanceAPIManager:
             return base_fee * 0.75
         return base_fee
 
-    def get_all_market_tickers(self):
+    def get_all_market_tickers(self) -> AllTickers:
         """
         Get ticker price of all coins
         """
-        return self.binance_client.get_all_tickers()
+        return AllTickers(self.binance_client.get_all_tickers())
 
     def get_market_ticker_price(self, ticker_symbol: str):
         """
@@ -135,16 +143,14 @@ class BinanceAPIManager:
 
         return order_status
 
-    def buy_alt(self, origin_coin: Coin, target_coin: Coin, all_tickers):
+    def buy_alt(self, origin_coin: Coin, target_coin: Coin, all_tickers: AllTickers):
         return self.retry(self._buy_alt, origin_coin, target_coin, all_tickers)
 
     def _buy_quantity(
         self, origin_symbol: str, target_symbol: str, target_balance: float = None, from_coin_price: float = None
     ):
         target_balance = target_balance or self.get_currency_balance(target_symbol)
-        from_coin_price = from_coin_price or get_market_ticker_price_from_list(
-            self.get_all_market_tickers(), origin_symbol + target_symbol
-        )
+        from_coin_price = from_coin_price or self.get_all_market_tickers().get_price(origin_symbol + target_symbol)
 
         origin_tick = self.get_alt_tick(origin_symbol, target_symbol)
         return math.floor(target_balance * 10 ** origin_tick / from_coin_price) / float(10 ** origin_tick)
@@ -159,7 +165,7 @@ class BinanceAPIManager:
 
         origin_balance = self.get_currency_balance(origin_symbol)
         target_balance = self.get_currency_balance(target_symbol)
-        from_coin_price = get_market_ticker_price_from_list(all_tickers, origin_symbol + target_symbol)
+        from_coin_price = all_tickers.get_price(origin_symbol + target_symbol)
 
         order_quantity = self._buy_quantity(origin_symbol, target_symbol, target_balance, from_coin_price)
         self.logger.info(f"BUY QTY {order_quantity}")